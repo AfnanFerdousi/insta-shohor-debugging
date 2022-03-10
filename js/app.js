0a4bdab6855b1e172369f3a870a0cbb07eef4a81
AfnanFerdousi <afnanferdousi550@gmail.com>
2022-03-10 21:39:25 +0600

    description read more solved

--- js/app.js
+++ js/app.js
@@ -103,7 +103,7 @@ const createPost = (post) => {
                   </button>
                 </div>
 
-                <div class="post__content">${displayContent(post.description)}</div>
+                <div class="post__content">${displayContent(pos.description)}</div>
 
                 <div class="post__infos">
                   <div class="post__likes">
